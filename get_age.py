441a6eb89c4b2cad8faae21056ae42dd29c93a7f
d00485618 <d00485618@utahtech.edu>
2026-01-20 10:08:08 -0700

    first commit

---
+++ get_age.py
@@ -0,0 +1,14 @@
+import random
+
+def getUserAge():
+    guessedRight = False
+    print("Hello, I am going to try and guess your age.")
+
+    userName = input("What is your name (first name, last name): ")
+    while guessedRight != True:
+        askUser = input("Are you " + str(random.randint(15, 30)) + " years old? (y/n): ")
+        if askUser == y:
+            pass
+
+
+    userInput = input("")

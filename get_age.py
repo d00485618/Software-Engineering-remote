d801e4e6db09a00de4f1f997384f1afd70267a19
d00485618 <d00485618@utahtech.edu>
2026-01-20 10:31:33 -0700

    fixed guesser from repeating already guessed ages

--- get_age.py
+++ get_age.py
@@ -2,14 +2,23 @@ import random
 
 def getUserAge():
     guessedRight = False
+    guessedAges = [0]
+    randomNum = 0
+
     print("Hello, I am going to try and guess your age.")
 
     userName = input("What is your name (first name, last name): ")
+
     while guessedRight != True:
-        randomNum = random.randint(15, 30)
+
+        while randomNum in guessedAges:
+            randomNum = random.randint(15, 30)
+        guessedAges.append(randomNum)
+
         askUser = input("Are you " + str(randomNum) + " years old? (y/n): ")
+        
         if askUser == "y":
-            print(str(userName) + " is " + str(randomNum) + "years old.")
+            print(str(userName) + " is " + str(randomNum) + " years old.")
             guessedRight = True
         else:
             print("Rats!")
